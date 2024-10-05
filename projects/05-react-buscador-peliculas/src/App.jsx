f19ef20bb745d00bd826234091e59d259bb801a9
Martin Egea <mar_egea@hotmail.com>
2024-10-05 17:07:30 -0300

    aplicación del useRef, custom hooks y mocks de una api

---
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -0,0 +1,77 @@
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import { useMovies } from "./hooks/useMovies";
+import { Movies } from "./components/Movies";
+
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+
+    if (search === "") {
+      setError("no se puede hacer una busqueda vacia");
+      return;
+    }
+
+    if (search.match(/^[0-9]+$/)) {
+      setError("la busqueda no puede ser un numero");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("la busqueda debe ser mayor a 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
+
+function App() {
+  const { mappedMovies } = useMovies();
+  const { search, updateSearch, error } = useSearch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(search);
+  };
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value);
+  };
+
+  return (
+    <div className="page">
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: "1px solid transparent",
+              borderColor: error ? "red" : "transparent",
+            }}
+            onChange={handleChange}
+            value={search}
+            placeholder="Avengers, Star Wars, etc"
+          />
+          <button type="submit">Buscar</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </header>
+
+      <main>
+        <Movies movies={mappedMovies} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
