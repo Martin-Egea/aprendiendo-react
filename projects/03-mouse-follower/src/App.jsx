47969cba6d05b1e04986c2564d0277dfdd3ceed0
Martin Egea <mar_egea@hotmail.com>
2024-10-05 17:08:41 -0300

    un espacio XD

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react"
 
-
 const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
