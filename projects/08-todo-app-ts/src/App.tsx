280c5da073558ea51881c58940e9a5ddee42a594
Martin Egea <mar_egea@hotmail.com>
2025-07-28 23:31:36 -0300

    agregando algunas props y  accion de remover un todo

--- projects/08-todo-app-ts/src/App.tsx
+++ projects/08-todo-app-ts/src/App.tsx
@@ -22,9 +22,14 @@ const mockTodos = [
 const App = () => {
   const [todos, setTodos] = useState(mockTodos)
 
+  const handleRemove = (id: number) => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos);
+  }
+
   return (
     <div className="todoapp">
-      <Todos todos={todos}></Todos>
+      <Todos todos={todos} onRemoveTodo={handleRemove}/>
     </div>
   )
 }
--- projects/08-todo-app-ts/src/components/Todo.tsx
+++ projects/08-todo-app-ts/src/components/Todo.tsx
@@ -1,13 +1,15 @@
 import { type Todo as TodoType } from "../types"
 
-type Props = TodoType;
+interface Props extends TodoType {
+    onRemoveTodo: (id: number) => void
+}
 
-export const Todo: React.FC<Props> = ({ id, title, completed}) => {
+export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo}) => {
     return (
         <div className="view">
             <input type="checkbox" className="toggle" checked={completed} onChange={() => {}} />
             <label>{title}</label>
-            <button className="destroy" onClick={() => {}}/>
+            <button className="destroy" onClick={() => {onRemoveTodo(id)}}/>
         </div>
 
     )
